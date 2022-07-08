eff1989e0dc5e2cff5be65fc8d706e5682086232
Marta Molina <marta.molina@live.u-tad.com>
2022-07-09 00:30:44 +0200

    OkHttp and GsonConverter

--- app/src/main/java/com/marta/rickmortyappjava/api/NetworkObj.java
+++ app/src/main/java/com/marta/rickmortyappjava/api/NetworkObj.java
@@ -1,11 +1,23 @@
 package com.marta.rickmortyappjava.api;
 
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Converter;
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 
 public class NetworkObj {
     private NetworkObj myNetWorkObj = null;
     public static final String BASE_URL = "https://rickandmortyapi.com/api/";
     private NetworkService service;
+    private OkHttpClient client;
+
+    private NetworkObj() {
+        buildClient();
+        buildRetrofit();
+    }
 
     public NetworkObj getInstance() {
         if (myNetWorkObj == null) {
@@ -14,15 +26,25 @@ public class NetworkObj {
         return myNetWorkObj;
     }
 
-    private NetworkObj() {
-        buildRetrofit();
-    }
-
     private void buildRetrofit() {
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
                 .build();
         this.service = retrofit.create(NetworkService.class);
+    }
+
+    private HttpLoggingInterceptor getInterceptor() {
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.level(HttpLoggingInterceptor.Level.BODY);
+        return interceptor;
+    }
+
+    private void buildClient() {
+        client = new OkHttpClient.Builder()
+                .addInterceptor(getInterceptor())
+                .build();
     }
 
     public NetworkService getService() {

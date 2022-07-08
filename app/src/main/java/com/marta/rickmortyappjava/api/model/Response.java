944b186b6fe59228aee025665e91ef61fd45fbff
Marta Molina <marta.molina@live.u-tad.com>
2022-07-08 23:45:12 +0200

    Parsing API response

---
+++ app/src/main/java/com/marta/rickmortyappjava/api/model/Response.java
@@ -0,0 +1,7 @@
+package com.marta.rickmortyappjava.api.model;
+
+import java.util.ArrayList;
+
+public class Response {
+        ArrayList< ResultResponse > results = new ArrayList < ResultResponse > ();
+}
---
+++ app/src/main/java/com/marta/rickmortyappjava/api/model/ResultResponse.java
@@ -0,0 +1,88 @@
+package com.marta.rickmortyappjava.api.model;
+
+public class ResultResponse {
+    private float id;
+    private String name;
+    private String status;
+    private String species;
+    private String type;
+    private String gender;
+    private String image;
+    private String url;
+    private String created;
+
+    // Getter Methods
+    public float getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getCreated() {
+        return created;
+    }
+
+    // Setter Methods
+
+    public void setId(float id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void setCreated(String created) {
+        this.created = created;
+    }
+}
